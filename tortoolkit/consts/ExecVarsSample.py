f2c94bd094d3478dfe3d30bed60fa166fabd98a2
Ilham Rofi'i <70249693+ilhamr0f11@users.noreply.github.com>
2022-05-09 16:03:09 +0700

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -37,10 +37,7 @@ except:
         REMAINING_STR = "▱"
 
         # DB URI for access
-        DATABASE_URL = (
-            "dbname=evoaeqss user=evoaeqss password=pao4EuAaahxP4WVQAGNIqxyKj0fVLaDv host=127.0.0.1 port=5432"
-            
-        )
+        DATABASE_URL = "dbname=evoaeqss user=evoaeqss password=pao4EuAaahxP4WVQAGNIqxyKj0fVLaDv host=127.0.0.1 port=5432"
 
         # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
         # DATABASE_URL = "dbname=tortk user=postgres password=your-pass host=db port=5432"
